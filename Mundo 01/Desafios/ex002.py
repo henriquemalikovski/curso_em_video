e46cd621f51bffeca958a1b1049ec8e5e34310d7
Henrique Malikovski <hmalikovski@gmail.com>
2022-08-07 21:14:28 -0300

    Arquivos das aulas

---
+++ Mundo 01/Desafios/ex002.py
@@ -0,0 +1,5 @@
+dia = input('Digite o dia: ');
+mes = input('Digite o mês: ');
+ano = input('Digite o ano: ');
+
+print('Você nasceu em ', dia,mes,ano, '. Correto?')
--- Mundo 01/desafio_02.py
+++
@@ -1,5 +0,0 @@
-dia = input('Digite o dia: ');
-mes = input('Digite o mês: ');
-ano = input('Digite o ano: ');
-
-print('Você nasceu em ', dia,mes,ano, '. Correto?')
